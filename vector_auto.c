dbb77b212e2db1e9531d08aff29cf774e6be81d1
Francisco Souza <f@souza.cc>
2013-06-27 00:19:30 -0300

    vector_auto: exemplo com auto vectorization

---
+++ vector_auto.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void
+multiply(int *a, int *b, int *c, int n)
+{
+	int i;
+#pragma simd
+	for(i = 0; i < n; i++) {
+		c[i] = a[i] * b[i];
+	}
+}
+
+int
+main(void)
+{
+	int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int b[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+	int i, c[8];
+	multiply(a, b, c, 8);
+	for(i = 0; i < 8; i++) {
+		printf("%d ", c[i]);
+	}
+	printf("\n");
+	return 0;
+}
