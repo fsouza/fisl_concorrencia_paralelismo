2b3d5908c1aac1484586c28e19dc5e97a446bf23
Francisco Souza <f@souza.cc>
2013-07-11 00:01:17 -0300

    all: adicionando banners de copyright

--- Increment.java
+++ Increment.java
@@ -1,3 +1,7 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Increment {
--- pingpong.erl
+++ pingpong.erl
@@ -1,3 +1,7 @@
+%% Copyright 2013 Francisco Souza. All rights reserved.
+%% Use of this source code is governed by a BSD-style
+%% license that can be found in the LICENSE file.
+
 -module(pingpong).
 
 -export([start_ping/1, start_pong/0,  ping/2, pong/0]).
